3ca096850556c91af6aa9e46b511cecb8da51bb8
Ulises <uli.atx@gmail.com>
2019-11-19 15:27:18 -0600

    alignment

--- src/components/BlackKey.js
+++ src/components/BlackKey.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function BlackKey (props) {
     return (
-        <div>
+        <div className="black-key" id={"black-" + props.index}>
             {props.myKey}
         </div>
     )
--- src/components/Octave.js
+++ src/components/Octave.js
@@ -3,7 +3,6 @@ import BlackKey from "./BlackKey";
 import WhiteKey from "./WhiteKey";
 
 export default function Octave(props) {
-  // const [width, setWidth] = useState(props.width)
   const [key, setKey] = useState("C");
   const [whiteKeys, setWhiteKeys] = useState(
     ["C", "D", "E", "F", "G", "A", "B"]);
@@ -12,20 +11,36 @@ export default function Octave(props) {
       sharps: ["C♯", "D♯", "F♯", "G♯", "A♯"], 
       flats: ["D♭", "E♭", "G♭", "A♭", "B♭"]
     })
+  const [useSharps, setUseSharps] = useState(true);
+
+  const calcLeftValues = (width) => {
+
+  }
+  
+  const toggleSharps = () => {
+    useSharps ? setUseSharps(false) : setUseSharps(true);
+  }
 
   return (
-    <div id="piano" style={{width: props.width + "px"}}>
-      {
-      whiteKeys.map(each => {
-        return <WhiteKey key={each} myKey={each}></WhiteKey>;
-      })
-      }
-      {
-      blackKeys.sharps.map(each => {
-        return <BlackKey key={each} myKey={each} id="black"></BlackKey>;
-      })
-      }
-      
+    <div>
+      <div id="piano" style={{width: props.width + "px"}}>
+        {
+        whiteKeys.map(each => {
+          return <WhiteKey key={each} myKey={each}></WhiteKey>;
+        })
+        }
+        {
+        useSharps ? 
+          blackKeys.sharps.map((each, index) => {
+            return <BlackKey key={each} myKey={each} index={index}></BlackKey>;
+          }) :
+          blackKeys.flats.map((each, index) => {
+            return <BlackKey key={each} myKey={each} index={index}></BlackKey>
+          })
+        }
+      </div>
+      <button onClick={toggleSharps}>Toggle Sharps</button>
     </div>
+    
   );
 }
